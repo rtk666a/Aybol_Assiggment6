fde101c8bb71a2044543874f06ad21710cdc581e
RTK <126328596+rtk666a@users.noreply.github.com>
2025-05-19 20:41:27 -0600

    change the packge name and tring the brench

--- src/com/coderscampus/Assigment6/Models.java
+++ src/com/coderscampus/Assigment6/Models.java
@@ -1,7 +1,18 @@
 package com.coderscampus.Assigment6;
 
 public enum Models {
-    model3,
-    modelS,
-    modelX,
+    model3("Model 3"),
+    modelS( "Model S"),
+    modelX( "Model X");
+
+    private final String displayName;
+
+    Models(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
--- src/com/coderscampus/Assigment6/TeslaApplication.java
+++ src/com/coderscampus/Assigment6/TeslaApplication.java
@@ -6,15 +6,16 @@ import java.util.Map;
 
 public class TeslaApplication {
     public static void main(String[] args) {
+        System.out.println("Assigment6");
         SalesService salesService = new SalesService();
         for (Models model : Models.values()) {
             List<TeslaSalesRecord> salesRecords = salesService.loadData(model);
             Map<Integer, Integer> yearlySales = salesService.collectYearlySales(salesRecords);
-            System.out.println(model.name() +" Yearly Sales Report" +
+            System.out.println(model +" Yearly Sales Report" +
                                              "\n--------------------------");
             yearlySales.forEach((year, sales) -> System.out.println( year + " -> " + sales));
-            System.out.println("The best month for  "  + model.name() + " : " + salesService.getBestMonth(salesRecords));
-            System.out.println("The worst month for  " + model.name() + " : " + salesService.getWorstMonth(salesRecords) + "\n");
+            System.out.println("The best month for  "  + model + " was : " + salesService.getBestMonth(salesRecords));
+            System.out.println("The worst month for  " + model + " was : " + salesService.getWorstMonth(salesRecords) + "\n");
         }
             
     }
